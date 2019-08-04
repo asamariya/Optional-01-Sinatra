4e4309f46fd6308d6d1e4c16630bcfc43877b5fe
Abhishek Samariya <asamariya@gmail.com>
2019-08-04 19:28:55 +1000

    stylesheet not connecting

--- app.rb
+++ app.rb
@@ -4,7 +4,8 @@ require "pry-byebug"
 require "better_errors"
 configure :development do
   use BetterErrors::Middleware
-  BetterErrors.application_root = File.expand_path('..', __FILE__)
+  # BetterErrors.application_root = File.expand_path('..', __FILE__)
+  BetterErrors.application_root = File.expand_path(__dir__)
 end
 
 require_relative 'lib/cookbook.rb'
@@ -23,7 +24,12 @@ end
 
 post '/recipes' do
   cookbook = Cookbook.new(File.join(__dir__, 'lib/recipes.csv'))
-  recipe = Recipe.new(name: params[:name], description: params[:description], difficulty: params[:difficulty], prep_time: params[:prep_time])
+  recipe = Recipe.new(
+    name: params[:name],
+    description: params[:description],
+    difficulty: params[:difficulty],
+    prep_time: params[:prep_time]
+  )
   cookbook.add_recipe(recipe)
   redirect to '/'
 end
--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,20 +1,19 @@
 class Recipe
-    attr_reader :name, :description, :path, :prep_time, :difficulty
-    def initialize(attributes = {})
-      @name = attributes[:name]
-      @description = attributes[:description]
-      @path = attributes[:path]
-      @prep_time = attributes[:prep_time]
-      @done = attributes[:done] == "true"
-      @difficulty = attributes[:difficulty]
-    end
-  
-    def done?
-      @done
-    end
-  
-    def mark_as_done!
-      @done = true
-    end
+  attr_reader :name, :description, :path, :prep_time, :difficulty
+  def initialize(attributes = {})
+    @name = attributes[:name]
+    @description = attributes[:description]
+    @path = attributes[:path]
+    @prep_time = attributes[:prep_time]
+    @done = attributes[:done] == "true"
+    @difficulty = attributes[:difficulty]
   end
-  
+
+  def done?
+    @done
+  end
+
+  def mark_as_done!
+    @done = true
+  end
+end
